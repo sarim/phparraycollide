0b86378e5f2cff3386bb7ed1feca8ab7bb657206
Sarim Khan <sarim2005@gmail.com>
2012-01-08 21:42:01 +0600

    sample output and colid class

---
+++ colid.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+
+
+/**
+ *
+ * @author sarim
+ */
+public class colid {
+   static String fileToString(String filename) throws IOException
+{
+    BufferedReader reader = new BufferedReader(new FileReader(filename));
+    StringBuilder builder = new StringBuilder();
+    String line;    
+
+    // For every line in the file, append it to the string builder
+    while((line = reader.readLine()) != null)
+    {
+        builder.append(line);
+    }
+
+    return builder.toString();
+}
+    public static void main(String arg[]) throws Exception{
+       
+//the output from generatedate stored in txt file
+        
+String data = fileToString("/Users/sarim/data.txt");
+
+try {
+    // Construct data
+
+    // Send data
+    URL url = new URL("http://www.example.com/");
+    URLConnection conn = url.openConnection();
+    conn.setDoOutput(true);
+    OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+    wr.write(data);
+    wr.flush();
+    //upload finished, now banging started on remote server
+System.out.println("upload end");
+    // lets wait from the response
+    BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    String line;
+    while ((line = rd.readLine()) != null) {
+        System.out.println(line);
+    }
+    wr.close();
+    rd.close();
+} catch (Exception e) {
+}        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
